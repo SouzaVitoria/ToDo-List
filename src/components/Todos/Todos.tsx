7b04465f89fc1109780134cbff6469cf80d8a256
SouzaVitoria <viihsouza161@gmail.com>
2022-07-25 14:47:34 -0300

    added to-do summary - styles component

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,22 +1,25 @@
 import { ClipboardText } from "phosphor-react";
+import styles from "./Todos.module.css"
 
 export function Todos() {
   return (
     <div className="container">
-      <div className="summaryTodos">
-        <div>
-          Tarefas criadas <strong> 0 </strong>
+      <div className={styles.todos}>
+        <div className={styles.summaryTodos}>
+          <p>
+            Tarefas criadas <strong> 0 </strong>
+          </p>
+          <p>
+            Concluídas <strong> 0 </strong>
+          </p>
         </div>
-        <div>
-          Concluídas <strong> 0 </strong>
-        </div>
-      </div>
 
-      <div className="nothingToDo">
-        <ClipboardText size={56} />
-        <div className="messages">
-          <strong> Você ainda não tem tarefas cadastradas </strong>
-          <p> Crie tarefas e organize seus itens a fazer </p>
+        <div className="nothingToDo">
+          <ClipboardText size={56} />
+          <div className="messages">
+            <strong> Você ainda não tem tarefas cadastradas </strong>
+            <p> Crie tarefas e organize seus itens a fazer </p>
+          </div>
         </div>
       </div>
     </div>
