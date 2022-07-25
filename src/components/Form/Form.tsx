b76b1d2377e1fca57351ce53f454d976e0d3c122
SouzaVitoria <viihsouza161@gmail.com>
2022-07-25 14:08:41 -0300

    added container in Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,12 +3,14 @@ import styles from "./Form.module.css"
 
 export function Form() {
   return (
-    <form className={styles.form}>
-      <input placeholder="Adicione uma nova tarefa" />
-      <button>
-        Criar
-        <PlusCircle size={32} />
-      </button>
-    </form>
+    <div className="container">
+      <form className={styles.form}>
+        <input placeholder="Adicione uma nova tarefa" />
+        <button>
+          Criar
+          <PlusCircle size={32} />
+        </button>
+      </form>
+    </div>
   )
 }
