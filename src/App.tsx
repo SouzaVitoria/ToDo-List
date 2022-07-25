bc48ee28ce5e224f23dcc32523e96b9861ee1e5e
SouzaVitoria <viihsouza161@gmail.com>
2022-07-25 14:08:06 -0300

    added Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./global.css"
 import { Header } from "./components/Header/Header"
+import { Form } from "./components/Form/Form"
 
 export function App() {
   return (
     <div>
       <Header />
+      <Form />
     </div>
   )
 }
