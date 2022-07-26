df419aefd84654c6a04087b1eab877fe38a9cd89
SouzaVitoria <viihsouza161@gmail.com>
2022-07-26 13:00:03 -0300

    added Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./global.css"
 import { Header } from "./components/Header/Header"
 import { Todos } from "./components/Todos/Todos"
+import { Footer } from "./components/Footer/Footer"
 
 export function App() {
   return (
     <div>
       <Header />
       <Todos />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import { Heart } from "phosphor-react";
+import styles from "./Footer.module.css"
+
+export function Footer() {
+  return (
+    <footer className={`${styles.footer}`}>
+      <p>
+        Desenvolvido por
+        <a href="https://github.com/SouzaVitoria/">Vitória Souza</a>
+        <Heart weight="fill" className={styles.heart} />
+      </p>
+    </footer>
+  )
+}
